0ab615e4c0b504ac00caef8cd68505f6cc67104c
mckay <mckaybwarren@gmail.com>
2023-02-16 08:39:05 -0500

    connect customer signup form to endpoint for creating a new customer

--- src/components/CustomerSignUpForm/CustomerSignUpForm.jsx
+++ src/components/CustomerSignUpForm/CustomerSignUpForm.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+
+const transformRequest = (values) => {
+  const {
+    firstName: first_name,
+    lastName: last_name,
+    email,
+    username,
+    password,
+    address1: address_1,
+    city,
+    region,
+    postalCode: postal_code,
+  } = values;
+  return {
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+    address_1,
+    city,
+    region,
+    postal_code,
+  };
+};
 
 const CustomerSignUpForm = () => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
@@ -17,20 +48,42 @@ const CustomerSignUpForm = () => {
     validationSchema: Yup.object({
       firstName: Yup.string().required("Required").max(20, "max 20 characters"),
       lastName: Yup.string().required("Required").max(20, "max 20 characters"),
-      email: Yup.string().email("Invalid email address").required("Required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Required")
+        .max(80, "max 80 characters"),
       username: Yup.string().required("Required").max(20, "max 20 characters"),
       password: Yup.string().required("Required"),
-      address1: Yup.string().required("Required"),
-      city: Yup.string().required("Required"),
-      region: Yup.string().required("Required"),
-      postalCode: Yup.string().required("Required"),
+      address1: Yup.string().required("Required").max(80, "max 80 characters"),
+      city: Yup.string().required("Required").max(80, "max 80 characters"),
+      region: Yup.string().required("Required").max(80, "max 80 characters"),
+      postalCode: Yup.string()
+        .required("Required")
+        .min(5, "Must be exactly 5 digits")
+        .max(5, "Must be exactly 5 digits"),
     }),
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      const requestBody = transformRequest(values);
+      axios
+        .post(
+          `${process.env.REACT_APP_BACKEND_URL}/customers/signup`,
+          requestBody
+        )
+        .then((response) => {
+          console.log(response);
+          alert("user created! returning to sign in page");
+          navigate("/login");
+        })
+        .catch((e) => {
+          console.log(e);
+          setError(e.response.data.message);
+        });
     },
   });
   return (
     <form onSubmit={formik.handleSubmit} className="form">
+      {error ? <span className="errors">{error}</span> : null}
+
       <label htmlFor="firstName">First Name: </label>
       <input
         id="firstName"
